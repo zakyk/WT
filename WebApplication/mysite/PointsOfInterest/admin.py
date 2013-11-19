75d184ed2795274cfe60ccfe84da263a182b2490
zakyk <michalzakarovsky@gmail.com>
2013-11-19 21:47:51 +0100

    aktualna verzia

--- WebApplication/mysite/PointsOfInterest/admin.py
+++ WebApplication/mysite/PointsOfInterest/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from PointsOfInterest.models import User
+from PointsOfInterest.models import Friend
+from PointsOfInterest.models import Photo
+
+
 from PointsOfInterest.models import CategoryOfPoi
 from PointsOfInterest.models import PointOfInterest
 
 admin.site.register(User)
+admin.site.register(Friend)
 admin.site.register(CategoryOfPoi)
+admin.site.register(Photo)
 admin.site.register(PointOfInterest)
 # Register your models here.
--- WebApplication/mysite/PointsOfInterest/models.py
+++ WebApplication/mysite/PointsOfInterest/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-//komentar
-
-
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=50)
